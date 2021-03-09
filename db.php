dd44e0ac72cb54d112a96950827920342729beaf
THANAWANPHATPITAK <6306021422151@fitm.kmutmb.ac.th>
2021-03-09 12:11:17 +0700

    create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+    define("hostname","localhost");
+    define("user","Golf");
+    define("password","XjM7zhOoVfCD0ub8");
+    define("dbname","bookstore");
+
+?>
+
+<?php
+
+    $conn=new mysqli(hostname,user,password,dbname);
+    if(!$conn) die ("ติดต่อฐานข้อมูลไม่ได้".$conn-> connection);
+    else echo"DB connected";
+?>
